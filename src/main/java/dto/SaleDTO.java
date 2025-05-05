65c36c042282206f55ad3b53592bf39eb1a2c86c
Nesrin Asbai <nesrinasbai05@gmail.com>
2025-05-05 09:21:20 +0200

    Update SaleDTO.java
    Accidentally uploaded old version of code

--- src/main/java/dto/SaleDTO.java
+++ src/main/java/dto/SaleDTO.java
@@ -2,6 +2,14 @@ package dto;
 
 import java.util.ArrayList;
 
+/**
+ * A DTO representing a completed sale. It's used to transfer sale information without exposing internal model logic.
+ * @param totalPrice The total price of the sale, including VAT.
+ * @param totalVAT The total VAT amount for the sale.
+ * @param items A list of items included in the sale.
+ * @param cash The amount of cash paid by the customer.
+ * @param change The amount of change to be returned to the customer.
+ */
 public record SaleDTO(float totalPrice, float totalVAT, ArrayList<ItemDTO> items, float cash, float change) {
 
 }
