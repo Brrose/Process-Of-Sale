52210b2d25f311bc3fcc1ef03405326ad989c4f1
Nesrin Asbai <nesrinasbai05@gmail.com>
2025-05-05 09:21:49 +0200

    Update Inventory.java
    Accidentally uploaded old version of code

--- src/main/java/integration/Inventory.java
+++ src/main/java/integration/Inventory.java
@@ -3,22 +3,43 @@ package integration;
 import java.util.HashMap;
 import dto.ItemDTO;
 
+/**
+ * The {@code Inventory} class acts as an external inventory system.
+ * It provides methods to check if an item is valid and retrieve item data.
+ */
 public class Inventory {
     
     private HashMap<String, ItemDTO> inventory = new HashMap<>();
-
+    
+    /**
+     * Creates a new {@code Inventory} instance and initializes it with a sample of items.
+     */
     public Inventory() {
         createItems();
     }
-
+    
+    /**
+     * Checks if an item exists in the inventory.
+     * @param id The identifier of the item being checked.
+     * @return {@code true} if the item exists, {@code false} if not.
+     */
     public boolean isValidItem(String id) {
         return inventory.get(id) != null;
     }
-
+    
+    /**
+     * Retrieves an item from the inventory with its identifier.
+     * @param id The identifier of the item being retrieved.
+     * @return The {@link ItemDTO} corresponding to the identifier, or {@code null} if not found.
+     */
     public ItemDTO getItem(String id) {
         return inventory.get(id);
     }
-
+    
+    /**
+     * Fills the inventory with a set of items.
+     * The method is called when the inventory is initialized.
+     */
     private void createItems() {
         inventory.put("abc123", new ItemDTO("abc123", "BigWheel Oatmeal 500g, whole grain oats, high fiber, gluten free", "BigWheel Oatmeal", 29.90f, 6f));
         inventory.put("def456", new ItemDTO("def456", "YouGoGo Blueberry 240g, low sugar youghurt, blueberry flavour", "YouGoGo Blueberry", 14.90f, 6f));
