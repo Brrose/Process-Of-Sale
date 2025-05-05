fc8a964c97810d4f2cefd19acd3ced500660db92
Nesrin Asbai <nesrinasbai05@gmail.com>
2025-05-05 09:23:30 +0200

    Update Main.java
    Accidentally uploaded old version of code

--- src/main/java/startup/Main.java
+++ src/main/java/startup/Main.java
@@ -1,8 +1,18 @@
 package startup;
+
 import controller.Controller;
 import view.View;
 
+/**
+ * The {@code Main} class serves as the start point for the program.
+ * It initializes the controller and view and runs a sample purchase.
+ */
 public class Main {
+    
+    /**
+     * Starts the program by creating the controller and view, then running a sample purchase.
+     * @param args 
+     */
     public static void main(String[] args) {
         Controller controller = new Controller();
         View view = new View(controller);
