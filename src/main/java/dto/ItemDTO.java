168bbfbc8fbdb7c9ee70bff1722f082928e52280
Nesrin Asbai <nesrinasbai05@gmail.com>
2025-05-05 09:20:56 +0200

    Update ItemDTO.java
    Accidentally uploaded old version of code.

--- src/main/java/dto/ItemDTO.java
+++ src/main/java/dto/ItemDTO.java
@@ -1,8 +1,12 @@
 package dto;
 
 /**
- *
- * @author nesri
+ * A DTO representing an item. Used to transfer item data without exposing internal model logic.
+ * @param id The unique identifier of the item.
+ * @param description A description of the item.
+ * @param name The name of the item.
+ * @param price The price of the item, excluding VAT.
+ * @param vat The VAT percentage applied to the item.
  */
 public record ItemDTO(String id, String description, String name, float price, float vat) {
 
